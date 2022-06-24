42f04e5610cc2d5261d926f918633a40217e6020
Ruslan Pimenov <pimenov.ruslan.99@gmail.com>
2022-06-25 03:45:15 +0700

    commit version 1

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,30 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
+    }
+
+    @Bean(name = "dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        return dog;
+    }
+
+    @Bean(name = "timer")
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        return timer;
+    }
 }
